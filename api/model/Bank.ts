b264601f4a7fafd7bf7e23b9bb86bc17eb7b1153
ValDev99 <99172923+ValDev99@users.noreply.github.com>
2025-06-18 11:43:28 +0200

    fix: add some code documentation

--- api/model/Bank.ts
+++ api/model/Bank.ts
@@ -7,14 +7,17 @@ type Account = {
 class Bank {
     private readonly accounts: Account[] = [];
 
+    // Méthode pour ouvrir un compte
     openAccount(accountNumber: string, clientName: string, initialDeposit: number = 0) {
         this.accounts.push({ accountNumber, clientName, balance: initialDeposit });
     }
 
+    // Méthode pour retirer de l'argent 
     withdrawal(accountNumber: string, clientName: string, amount: number): void {
         const account = this.accounts.find(
             acc => acc.accountNumber === accountNumber && acc.clientName === clientName
         );
+        //gestion des erreurs
         if (!account) {
             throw new Error("Compte ou nom client incorrect.");
         }
@@ -24,6 +27,7 @@ class Bank {
         account.balance -= amount;
     }
 
+    // Méthode pour déposer de l'argent
     deposit(accountNumber: string, clientName: string, amount: number): void {
         const account = this.accounts.find(
             acc => acc.accountNumber === accountNumber && acc.clientName === clientName
@@ -34,6 +38,7 @@ class Bank {
         account.balance += amount;
     }
 
+    // Méthode pour obtenir le solde d'un compte
     getBalance(accountNumber: string, clientName: string): number {
         const account = this.accounts.find(
             acc => acc.accountNumber === accountNumber && acc.clientName === clientName
@@ -43,10 +48,14 @@ class Bank {
         }
         return account.balance;
     }
+
+    // Méthode pour convertir des montants entre Euro et USD
     conversionFromEuro(amount: number): number {
         const conversionRate = 1.18; // Example conversion rate from Euro to USD
         return amount * conversionRate;
     }
+
+    // Méthode pour convertir des montants entre USD et Euro
     conversionToEuro(amount: number): number {
         const conversionRate = 1.18; // Example conversion rate from USD to Euro
         return amount / conversionRate;
